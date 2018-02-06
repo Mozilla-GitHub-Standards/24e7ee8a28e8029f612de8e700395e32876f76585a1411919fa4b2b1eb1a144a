1d15afb8eaf9dd5bc85e07ce61618ba042e659f9
Franziskus Kiefer <franziskuskiefer@gmail.com>
2018-02-06 09:14:25 +0100

    curl test

--- harvester/run_canary.sh
+++ harvester/run_canary.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Insert this run into the taskcluster index
-python /home/worker/canary-harvester/harvester/set_index.py
+# python /home/worker/canary-harvester/harvester/set_index.py
+curl -v -X PUT "http://taskcluster/index/v1/task/project.nss-nspr.canary-harvester-test.foo-test" -H 'Content-Type: application/json' -d "{\"taskId\":\"${TASK_ID}\", \"rank\":10, \"data\":{}, \"expires\":\"2019-02-05T17:12:45.495Z\"}"
 
 tlscanary scan -l 100
 
