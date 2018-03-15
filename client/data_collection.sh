9ad34201274121f04e6a43ee4fde9038ae9ffb32
Jonas Allmann <jallmann@mozilla.com>
2018-03-15 16:58:42 +0100

    Refined data_collection script.

--- client/data_collection.sh
+++ client/data_collection.sh
@@ -4,38 +4,53 @@
 # Downloads and extracts the artifacts. 
 
 # IMPORTANT: Extracted logfiles are big, e.g. ~3.5 gB per run. (Not anymore. Only first ~10)
-if [[ "$1" = ""  ]] || [[ "$2" = ""  ]]
+if [[ "$1" = ""  ]]
 then
-  echo "Please enter Start AND End date for artifact download."
+  echo "Please enter Start date for artifact download."
   echo "Date format 'YYYY-MM-DD'"
-  echo "  ./data_collection [start date (incl.)] [end date (excl.)]"
+  echo "  ./data_collection [start date (incl.)] [end date (excl.), opt.]"
   exit 1
 fi
 
-node main.js $1 $2 > urls.txt
+START=$1
+
+if [[ "$2" = ""  ]]
+then
+  END="2020-12-31"
+else  
+  END=$2
+fi
+
+node main.js $START $END > urls.txt
 
 sed -i '/got tasks.../d' urls.txt
 WORKDIR=$(pwd)
 if ! [[ -e "$WORKDIR/data" ]]
 then
   mkdir $WORKDIR/data
+  touch $WORKDIR/data/history
 fi
 
 COUNT=0
 for URL in $(cat urls.txt)
 do
-  echo "$URL"
-  mkdir tmp
-  cd tmp
-  wget "$URL"
-  bzip2 -d public%2Fcanarylog.tar.bz2
-  tar -xf public%2Fcanarylog.tar
-  cd .tlscanary/log/*/*/*
-  bzip2 -d log.bz2
-  mv $WORKDIR/tmp/.tlscanary/log/*/*/* $WORKDIR/data
-  cd $WORKDIR
-  rm -rf tmp
+  if ! grep -Fxq $URL $WORKDIR/data/history
+  then
+    echo "$URL" >> $WORKDIR/data/history
+    echo "$URL"
+    mkdir tmp
+    cd tmp
+    wget "$URL"
+    bzip2 -d public%2Fcanarylog.tar.bz2
+    tar -xf public%2Fcanarylog.tar
+    cd .tlscanary/log/*/*/*
+    bzip2 -d log.bz2
+    mv $WORKDIR/tmp/.tlscanary/log/*/*/* $WORKDIR/data
+    cd $WORKDIR
+    rm -rf tmp
+  fi
 done
+
 
 rm urls.txt
 
--- client/database.py
+++ client/database.py
@@ -10,6 +10,7 @@ import sqlite3
 import json
 import sys
 import os
+import random
 
 # Imports a logfile and the log metadata from a given folder.
 # Returns the array of JSON objects and a single JSON object containing metadata.
@@ -45,11 +46,12 @@ db_folder = source_folder + "database"
 
 log_folders = os.listdir(source_folder)
 
-if ("database" in log_folders):
-    log_folders.remove("database")
+random.seed()
 
 if not os.path.exists(db_folder):
     os.makedirs(db_folder)
+
+open(db_folder + "/history", 'a').close()
 
 db = sqlite3.connect(db_folder + "/logs.db")
 
@@ -58,18 +60,39 @@ c = db.cursor()
 for j in range(len(log_folders)):
     datetime = log_folders[j]
     
+    
+    
+    with open(db_folder + "/history", 'r') as hist:
+        if (datetime in hist.read()):
+            print("Skipping " + datetime + ". Already in database.")
+            continue
+
+    with open(db_folder + "/history", 'a') as hist:
+            hist.write(datetime + "\r\n")
+    
     print("Reading " + datetime + "...")
-    data, meta = import_logfolder(source_folder + datetime)
+    try:
+        data, meta = import_logfolder(source_folder + datetime)
+    except:
+        print("Skipping " + datetime)
+        continue
+
     
     print("Writing " + datetime + " to DB...")
     
-    c.execute('''CREATE TABLE IF NOT EXISTS log ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "host" TEXT, "rank" INTEGER, "response_time" REAL, "success" TEXT,
-                                                 "signature_scheme" TEXT, "cipher_name" TEXT, "protocol_version" TEXT, "cert_untrusted" TEXT, "error_class" TEXT, 
+    c.execute('''CREATE TABLE IF NOT EXISTS log ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,"run_id" INTEGER, "host" TEXT, "rank" INTEGER, "response_time" REAL, "connect_success" TEXT,
+                                                 "command_success" TEXT, "signature_scheme" TEXT, "cipher_name" TEXT, "protocol_version" TEXT, "cert_untrusted" TEXT, "error_class" TEXT, 
                                                  "raw_error" TEXT, "short_error" TEXT, "CA_name" TEXT, "date" TEXT)''')
     
-    c.execute('''CREATE TABLE IF NOT EXISTS testrun ("date" TEXT PRIMARY KEY, "max_timeout" INTEGER, "scans_per_host" INTEGER, "version" TEXT,
+    c.execute('''CREATE TABLE IF NOT EXISTS testrun ("run_id" INTEGER PRIMARY KEY NOT NULL, "date" TEXT, "max_timeout" INTEGER, "scans_per_host" INTEGER, "version" TEXT,
                                                  "build_ID" TEXT, "nspr_version" TEXT, "nss_version" TEXT,
                                                  "canary_version" TEXT)''')
+    
+    unique = False
+    while (not unique):
+        run_id = random.randint(100000, 999999)
+        unique = not (run_id,) in c.execute('''SELECT run_id FROM testrun''')
+    
     meta_date = meta['run_finish_time'][:10]
     meta_timeout = meta['args']['timeout']
     meta_scans = meta['args']['scans']
@@ -78,7 +101,7 @@ for j in range(len(log_folders)):
     meta_nspr = meta['test_metadata']['nspr_version']
     meta_nss = meta['test_metadata']['nss_version']
     meta_canary = meta['tlscanary_version']
-    c.execute("INSERT INTO testrun VALUES ('{0}',{1},{2},'{3}','{4}','{5}','{6}','{7}')".format(meta_date, meta_timeout, 
+    c.execute("INSERT INTO testrun VALUES ({0},'{1}',{2},{3},'{4}','{5}','{6}','{7}', '{8}')".format(run_id, meta_date, meta_timeout, 
                                           meta_scans, meta_version, meta_build, meta_nspr, meta_nss, meta_canary))
     
     for i in range(len(data)):
@@ -87,7 +110,8 @@ for j in range(len(log_folders)):
         request_time = response_time - command_time
         host = data[i]['host'].encode('UTF-8')
         rank = data[i]['rank']
-        success = data[i]['success']
+        command_success = data[i]['success']
+        connection_success = data[i]['response']['success']
         try:
             signature_scheme = data[i]['response']['result']['info']['ssl_status']['signatureSchemeName'].encode('UTF-8')
         except:
@@ -122,10 +146,10 @@ for j in range(len(log_folders)):
             CA_name = 'NA'
         date = meta['run_finish_time'][:10]   
         
-        command = "INSERT INTO log ('host', 'rank', 'response_time', 'success', 'signature_scheme', 'cipher_name', 'protocol_version', \
+        command = "INSERT INTO log ('run_id', 'host', 'rank', 'response_time', 'connect_success', 'command_success', 'signature_scheme', 'cipher_name', 'protocol_version', \
                             'cert_untrusted', 'error_class', 'raw_error','short_error', 'CA_name', 'date') \
-                            VALUES ('{0}',{1},{2},'{3}','{4}','{5}','{6}','{7}','{8}', '{9}', '{10}', '{11}', '{12}')".format(host, rank, 
-                                          request_time, success, signature_scheme, cipher_name, protocol_version, cert_untrusted, error_class,
+                            VALUES ({0},'{1}',{2},{3},'{4}','{5}','{6}','{7}','{8}', '{9}', '{10}', '{11}', '{12}','{13}', '{14}')".format(run_id, host, rank, 
+                                          request_time, connection_success, command_success, signature_scheme, cipher_name, protocol_version, cert_untrusted, error_class,
                                           raw_error, short_error, CA_name, date)
 
         try:
